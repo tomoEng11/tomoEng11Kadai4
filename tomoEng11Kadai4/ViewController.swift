aad03a4918801076cd423a35664a7d18460b913a
tomo <tom_friendship@me.com>
2023-12-10 15:04:36 +0900

    initial commit

--- tomoEng11Kadai4/ViewController.swift
+++ tomoEng11Kadai4/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var countLabel: UILabel!
+    private var countNumber: Int = 0
+
+    @IBAction func countUpButton(_ sender: UIButton) {
+        countNumber += 1
+        countLabel.text = String(countNumber)
     }
 
-
+    @IBAction func countClearButton(_ sender: UIButton) {
+        countNumber = 0
+        countLabel.text = String(countNumber)
+    }
 }
-
